e491fe52f90198199994daeb5ec97e7c1df3965f
MagnierHugo <magnierhugo@proton.me>
2025-01-15 20:01:34 +0100

    a

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,27 @@
 #include <iostream>
 
 #include "test.hpp"
+#include "Maths/matrix.hpp"
 
 
 int main(int argc, char* argv[]) {
 
-    utl::TestClass test{32};
+    // utl::TestClass test{32};
 
-    test.GetSetLog(test.Internal, "Internal Default");
+    // test.GetSetLog(test.Internal, "Internal Default");
 
-    test.GetSetLog(test.External, "External Default");
-    test.LogExternal();
+    // test.GetSetLog(test.External, "External Default");
+    // test.LogExternal();
+
+    // utl::Matrix<float, 1, 4> matrix;
+
+    int var = 3;
+    _ = var;
+
+    std::cout << "Var after voided: " << var << '\n';
+
+    int test = 4;
+    std::ignore = test;
     
     return 0;
 }
