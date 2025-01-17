f63523eaae38527a4a6beb52be961bedbaf3e660
MagnierHugo <magnierhugo@proton.me>
2025-01-17 09:09:20 +0100

    BIG

---
+++ include/Maths/big_number.hpp
@@ -0,0 +1,16 @@
+// Maths/big_number.hpp
+
+namespace utl {
+
+    template<>
+    class BigNumber {
+    public:
+        BigNumber() = default;
+        ~BigNumber();
+
+    private:
+
+
+    };
+
+}
